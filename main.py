1faccef74ad2fdd4bd161e30dca43a44a842c5ca
Jorge Correa <jorge_thiago1@hotmail.com>
2022-11-16 17:29:23 -0300

    segundo commit

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
+from kivy.core.window import Window
+Window.size = (400, 680)
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
+from kivy.uix.screenmanager import ScreenManager
 from kivymd.uix.button import MDRaisedButton
 
 class MDRaisedButton(MDRaisedButton):
     pass
 
-class Raiz(MDScreen):
+class Raiz(ScreenManager):
+    pass
+
+class StartScreen(MDScreen):
+    pass
+
+class StartInfo(MDScreen):
     pass
 
 class ForteApp(MDApp):
     def build(self):
         self.theme_cls.material_style = "M3"
         return Raiz()
+
+    def callback(self,*args):
+        print('here')
 
 if __name__ == '__main__':
     ForteApp().run()
